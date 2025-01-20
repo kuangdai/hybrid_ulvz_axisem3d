00564f343ff0eecc272d8f27f0d40c2fdd89527f
Kuangdai Leng <kuangdai.leng@stfc.ac.uk>
2025-01-20 14:25:05 +0000

    t1

--- wave_animation/process_outgoing.sh
+++ wave_animation/process_outgoing.sh
@@ -1,4 +1,4 @@
-python process_outgoing.py --input-path ~/Downloads/anim --t0 750 --t1 1050 --time-interval=1 --medium fluid --view side
-python process_outgoing.py --input-path ~/Downloads/anim --t0 750 --t1 1050 --time-interval=1 --medium solid --view side
-python process_outgoing.py --input-path ~/Downloads/anim --t0 750 --t1 1050 --time-interval=1 --medium fluid --view top
-python process_outgoing.py --input-path ~/Downloads/anim --t0 750 --t1 1050 --time-interval=1 --medium solid --view top
+python process_outgoing.py --input-path ~/Downloads/anim --t0 750 --t1 1000 --time-interval=1 --medium fluid --view side
+python process_outgoing.py --input-path ~/Downloads/anim --t0 750 --t1 1000 --time-interval=1 --medium solid --view side
+python process_outgoing.py --input-path ~/Downloads/anim --t0 750 --t1 1000 --time-interval=1 --medium fluid --view top
+python process_outgoing.py --input-path ~/Downloads/anim --t0 750 --t1 1000 --time-interval=1 --medium solid --view top
