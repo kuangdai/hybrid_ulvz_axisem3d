fix rotation

wave_animation/process_incident.py
@@ -83,8 +83,7 @@ if __name__ == "__main__":
     # Read data
     print("Reading raw data...")
     ds = AxiSEM3DSyntheticsLoader(in_path / f"04_solve_prem/output/stations/ANIM_INCIDENT_{args.medium.upper()}",
-                                  in_path / f"02_stations/STATIONS_ANIM_INCIDENT_{args.medium.upper()}",
-                                  meta["event_lat"], meta["event_lon"], src_spz=True)
+                                  in_path / f"02_stations/STATIONS_ANIM_INCIDENT_{args.medium.upper()}")
     t0_id = np.searchsorted(ds.times, args.t0)
     t1_id = np.searchsorted(ds.times, args.t1)
     anim_data = ds.get(start_time=t0_id, end_time=t1_id, time_interval=args.time_interval)

wave_animation/process_outgoing.py
@@ -46,8 +46,7 @@ if __name__ == "__main__":
     print("Reading raw data...")
     ds = AxiSEM3DSyntheticsLoader(
         in_path / f"12_solve_3d/output/stations/ANIM_{args.view.upper()}_{args.medium.upper()}",
-        in_path / f"02_stations/STATIONS_ANIM_{args.view.upper()}_{args.medium.upper()}",
-        meta["ulvz_lat"], meta["ulvz_lon"], src_spz=True)
+        in_path / f"02_stations/STATIONS_ANIM_{args.view.upper()}_{args.medium.upper()}")
     t0_id = np.searchsorted(ds.times, args.t0)
     t1_id = np.searchsorted(ds.times, args.t1)
     anim_data = ds.get(start_time=t0_id, end_time=t1_id, time_interval=args.time_interval)
@@ -62,16 +61,22 @@ if __name__ == "__main__":
     lat = st[:, 2].astype(float)
     lon = st[:, 3].astype(float)
     dep = st[:, 5].astype(float)
-    points = GeoPoints(np.array([lat, lon, dep / 1e3]).T)
+    phi_under_ulvz = np.zeros((len(grid_dist_anim), len(grid_depth_anim), len(grid_azim_anim)))
+    phi_under_ulvz[:, :, :] = grid_azim_anim[None, None, :]
+    points = GeoPoints(np.array([lat, lon, dep / 1e3]).T, phi_under_ulvz.reshape(-1))
 
     # Rotate
     print("Rotating...")
+    fr_frame = points.form_spz_frame(meta["ulvz_lat"], meta["ulvz_lon"])
+    fr_frame = fr_frame.reshape(len(grid_dist_anim),
+                                len(grid_depth_anim),
+                                len(grid_azim_anim), 3, 3).swapaxes(0, 1).reshape(-1, 3, 3)
     to_frame = points.form_RTZ_frame(meta["event_lat"], meta["event_lon"])
     to_frame = to_frame.reshape(len(grid_dist_anim),
                                 len(grid_depth_anim),
                                 len(grid_azim_anim), 3, 3).swapaxes(0, 1).reshape(-1, 3, 3)
     anim_data = anim_data.reshape(anim_data.shape[0], anim_data.shape[1], -1)
-    anim_data = rotate(anim_data, ds.frame, to_frame, args.batch_size, args.device)
+    anim_data = rotate(anim_data, fr_frame, to_frame, args.batch_size, args.device)
     anim_data = anim_data.reshape(anim_data.shape[0], anim_data.shape[1],
                                   len(grid_depth_anim),
                                   len(grid_dist_anim),

wave_animation/synthetics.py
@@ -5,11 +5,9 @@ import torch
 import tqdm
 import xarray as xr
 
-from geodetic import GeoPoints
-
 
 class AxiSEM3DSyntheticsLoader:
-    def __init__(self, nc_path, station_file, src_lat, src_lon, src_spz, in_memory=True):
+    def __init__(self, nc_path, station_file, in_memory=True):
         self.nc_path = Path(nc_path)
         self.station_file = station_file
 
@@ -17,10 +15,6 @@ class AxiSEM3DSyntheticsLoader:
         station_file_data = np.loadtxt(station_file, dtype=str)
         self.st_keys = np.char.add(np.char.add(station_file_data[:, 1], "."),
                                    station_file_data[:, 0])
-        lat = station_file_data[:, 2].astype(float)
-        lon = station_file_data[:, 3].astype(float)
-        dep = station_file_data[:, 5].astype(float)
-        self.st_points = GeoPoints(np.array([lat, lon, dep / 1e3]).T)
 
         # Build dictionaries to map between keys to ranks
         rank_station_data = np.loadtxt(self.nc_path / "rank_station.info", dtype=str, skiprows=1)
@@ -50,12 +44,6 @@ class AxiSEM3DSyntheticsLoader:
                     self.data_on_rank[rank] = np.array(ds["data"].values)
             else:
                 self.data_on_rank[rank] = xr.open_dataset(nc_file)["data"]
-
-        # Set up source frame
-        if src_spz:
-            self.frame = self.st_points.form_spz_frame(src_lat, src_lon)
-        else:
-            self.frame = self.st_points.form_RTZ_frame(src_lat, src_lon)
 
     def get(self, st_keys=None, start_time=None, end_time=None, time_interval=1):
         if st_keys is None: