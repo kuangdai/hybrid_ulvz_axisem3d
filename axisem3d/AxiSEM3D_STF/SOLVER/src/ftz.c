2f6a3dd14468b16ec440764508c19ea3cfc4a6f8
Kuangdai <kuangdai.leng@gmail.com>
2025-06-26 18:12:56 +0100

    Update ftz.c

--- axisem3d/AxiSEM3D_STF/SOLVER/src/ftz.c
+++ axisem3d/AxiSEM3D_STF/SOLVER/src/ftz.c
@@ -1,15 +1,15 @@
 // ftz.c
-// created by Kuangdai on 1-Apr-2016 
+// created by Kuangdai on 1-Apr-2016
 // To activate flush to zero for denormal float handling
 // http://stackoverflow.com/questions/9314534/why-does-changing-0-1f-to-0-slow-down-performance-by-10x
 
 
-#ifndef _CRAYC
+#if !defined(_CRAYC) && !defined(__APPLE__)
 #include <xmmintrin.h>
 #endif
 
 void set_ftz() {
-#ifndef _CRAYC
+#if !defined(_CRAYC) && !defined(__APPLE__)
     _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
     #ifndef NDEBUG
         _MM_SET_EXCEPTION_MASK(_MM_MASK_INEXACT | _MM_MASK_UNDERFLOW | _MM_MASK_DENORM);
