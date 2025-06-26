811c1b0d57a4ed8529e13bfd3d1159f1aff42b75
Kuangdai <kuangdai.leng@gmail.com>
2025-06-26 18:09:13 +0100

    fix apple

--- axisem3d/AxiSEM3D_SOLVER/SOLVER/src/shared/ftz.c
+++ axisem3d/AxiSEM3D_SOLVER/SOLVER/src/shared/ftz.c
@@ -1,25 +1,18 @@
-//
-//  ftz.c
-//  AxiSEM3D
-//
-//  Created by Kuangdai Leng on 1/6/19.
-//  Copyright © 2019 Kuangdai Leng. All rights reserved.
-//
+// ftz.c
+// created by Kuangdai on 1-Apr-2016
+// To activate flush to zero for denormal float handling
+// http://stackoverflow.com/questions/9314534/why-does-changing-0-1f-to-0-slow-down-performance-by-10x
 
-//  To activate flush to zero for denormal float handling
-//  http://stackoverflow.com/questions/9314534/why-does-changing-0-1f-to-0-slow-down-performance-by-10x
 
-#ifndef _CRAYC
+#if !defined(_CRAYC) && !defined(__APPLE__)
 #include <xmmintrin.h>
 #endif
 
 void set_ftz() {
-#ifndef _CRAYC
+#if !defined(_CRAYC) && !defined(__APPLE__)
     _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
-#ifndef NDEBUG
-    _MM_SET_EXCEPTION_MASK(_MM_MASK_INEXACT |
-                           _MM_MASK_UNDERFLOW |
-                           _MM_MASK_DENORM);
-#endif
+    #ifndef NDEBUG
+        _MM_SET_EXCEPTION_MASK(_MM_MASK_INEXACT | _MM_MASK_UNDERFLOW | _MM_MASK_DENORM);
+    #endif
 #endif
 }
